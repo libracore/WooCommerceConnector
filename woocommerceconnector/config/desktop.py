7ec1ac6d0f53fb9172b0338ca974955b24b54d01
Joelios <joel.mesmer@libracore.com>
2018-05-16 15:57:09 +0200

    added desktop.py

---
+++ woocommerceconnector/config/desktop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "WooCommerceConnector",
+			"color": "#9b5c8f",
+			"icon": "fa fa-shopping-cart",
+			"type": "module",
+			"label": _("WooCommerceConnector")
+		}
+	]
