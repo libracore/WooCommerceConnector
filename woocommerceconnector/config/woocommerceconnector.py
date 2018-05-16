7df46b9d088ef317a26f1fbaa512ce8da928c076
Joelios <joel.mesmer@libracore.com>
2018-05-16 16:06:31 +0200

    added woocommerceconnector.py

---
+++ woocommerceconnector/config/woocommerceconnector.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Settings"),
+			"icon": "fa fa-building",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "woocommerce Settings",
+					"label": _("WooCommerce Settings"),
+					"description": _("Settings"),
+				},
+			]
+		},
+		{
+			"label": _("Logs"),
+			"icon": "fa fa-building",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "woocommerce Log",
+					"label": _("Sync Logs"),
+					"description": _("Sync Logs"),
+				}
+			]
+		}
+	]
