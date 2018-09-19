348077c7d9eee64e146649f0f9700409e41e32a6
lasalesi <larsericmueller@gmail.com>
2018-09-19 12:07:43 +0200

    Update sync_orders.py
    minor bugfix

--- woocommerceconnector/sync_orders.py
+++ woocommerceconnector/sync_orders.py
@@ -200,8 +200,8 @@ def create_order(woocommerce_order, woocommerce_settings, company=None):
 def create_sales_order(woocommerce_order, woocommerce_settings, company=None):
 	#customer = frappe.db.get_value("Customer", {"woocommerce_customer_id": woocommerce_order.get("customer_id")}, "name")
 	#backup_customer = frappe.db.get_value("Customer", {"woocommerce_customer_id": "Guest of Order-ID: {0}".format(woocommerce_order.get("id"))}, "name")
-	customer = frappe.get_all("Customer", "filters": {["woocommerce_customer_id", "=", woocommerce_order.get("customer_id")]}, fields=['name'])
-	backup_customer = frappe.get_all("Customer", "filters": {["woocommerce_customer_id", "=", "Guest of Order-ID: {0}".format(woocommerce_order.get("id"))]}, fields=['name'])
+	customer = frappe.get_all("Customer", filters: [["woocommerce_customer_id", "=", woocommerce_order.get("customer_id")]], fields=['name'])
+	backup_customer = frappe.get_all("Customer", filters: [["woocommerce_customer_id", "=", "Guest of Order-ID: {0}".format(woocommerce_order.get("id"))]], fields=['name'])
 	# debug customer mismatch
 	frappe.log_error("customer_id: {id}, customer: {customer}, backup_customer: {backup_customer}\n{order}".format(
 		id=woocommerce_order.get("customer_id"), customer=customer, backup_customer=backup_customer,order=woocommerce_order))
