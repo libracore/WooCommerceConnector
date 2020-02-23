fc7b6270e04557c6cd18cad3ecf98233d7704e3a
Carsten Tschense <carsten.tschense@gmail.com>
2020-02-23 17:40:25 +0100

    Don't overwrite category when syncing products

--- woocommerceconnector/sync_products.py
+++ woocommerceconnector/sync_products.py
@@ -179,8 +179,6 @@ def sync_item_with_woocommerce(item, price_list, warehouse):
             "name": item.get("item_name"),
             "description": item.get("woocommerce_description") or item.get("web_long_description") or item.get("description"),
             "short_description": item.get("woocommerce_description") or item.get("web_long_description") or item.get("description"),
-            "categories" : [],
-            "images": []
     }
     item_data.update( get_price_and_stock_details(item, warehouse, price_list) )
 
