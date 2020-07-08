cd4dd3533c07269469d0db047f8fe141fb27315b
lasalesi <larsericmueller@gmail.com>
2020-07-08 08:36:23 +0200

    Update sync_customers.py
    Improved duplicate contact check and exception handling for Python 3

--- woocommerceconnector/sync_customers.py
+++ woocommerceconnector/sync_customers.py
@@ -101,7 +101,7 @@ def create_customer_address(customer, woocommerce_customer):
             }).insert()
 
         except Exception as e:
-            make_woocommerce_log(title=e.message, status="Error", method="create_customer_address", message=frappe.get_traceback(),
+            make_woocommerce_log(title=e, status="Error", method="create_customer_address", message=frappe.get_traceback(),
                     request_data=woocommerce_customer, exception=True)
 
     if shipping_address:
@@ -127,7 +127,7 @@ def create_customer_address(customer, woocommerce_customer):
             }).insert()
             
         except Exception as e:
-            make_woocommerce_log(title=e.message, status="Error", method="create_customer_address", message=frappe.get_traceback(),
+            make_woocommerce_log(title=e, status="Error", method="create_customer_address", message=frappe.get_traceback(),
                 request_data=woocommerce_customer, exception=True)
 
 def create_customer_contact(customer, woocommerce_customer):
@@ -144,22 +144,29 @@ def create_customer_contact(customer, woocommerce_customer):
                 "phone": woocommerce_customer["billing"]["phone"],
                 "is_primary_phone": 1
             })
-        frappe.get_doc({
-            "doctype": "Contact",
-            "first_name": woocommerce_customer["billing"]["first_name"],
-            "last_name": woocommerce_customer["billing"]["last_name"],
-            "email_id": woocommerce_customer["billing"]["email"],
-            "phone": woocommerce_customer["billing"]["phone"],
-            "links": [{
-                "link_doctype": "Customer",
-                "link_name": customer.name
-            }],
-            "email_ids": email_ids,
-            "phone_nos": phone_nos
-        }).insert()
+        # duplicate check before inserting
+        contact_matches = frappe.get_all("Contact", 
+                                         filters={'first_name': woocommerce_customer["billing"]["first_name"],
+                                                  'last_name': woocommerce_customer["billing"]["last_name"],
+                                                  'email_id': woocommerce_customer["billing"]["email"]}
+                                         fields=['name'])
+        if not contact_matches:
+            frappe.get_doc({
+                "doctype": "Contact",
+                "first_name": woocommerce_customer["billing"]["first_name"],
+                "last_name": woocommerce_customer["billing"]["last_name"],
+                "email_id": woocommerce_customer["billing"]["email"],
+                "phone": woocommerce_customer["billing"]["phone"],
+                "links": [{
+                    "link_doctype": "Customer",
+                    "link_name": customer.name
+                }],
+                "email_ids": email_ids,
+                "phone_nos": phone_nos
+            }).insert()
 
     except Exception as e:
-        make_woocommerce_log(title=e.message, status="Error", method="create_customer_contact", message=frappe.get_traceback(),
+        make_woocommerce_log(title=e, status="Error", method="create_customer_contact", message=frappe.get_traceback(),
                 request_data=woocommerce_customer, exception=True)
 
 def get_country_name(code):
