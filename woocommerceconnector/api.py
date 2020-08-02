fedf5dd89efdf6cd0dd627957540095ecc150dd1
lasalesi <larsericmueller@gmail.com>
2020-08-02 22:32:57 +0200

    hotfix indentation

--- woocommerceconnector/api.py
+++ woocommerceconnector/api.py
@@ -14,13 +14,13 @@ from frappe.utils.background_jobs import enqueue
 
 @frappe.whitelist()
 def check_hourly_sync():
-	woocommerce_settings = frappe.get_doc("WooCommerce Config")
-	if woocommerce_settings.hourly_sync == 1:
-		sync_woocommerce()
+    woocommerce_settings = frappe.get_doc("WooCommerce Config")
+    if woocommerce_settings.hourly_sync == 1:
+        sync_woocommerce()
 
 @frappe.whitelist()
 def sync_woocommerce():
-	"""Enqueue longjob for syncing woocommerce"""
+    """Enqueue longjob for syncing woocommerce"""
     woocommerce_settings = frappe.get_doc("WooCommerce Config")
     if woocommerce_settings.sync_timeout == 0:
         woocommerce_settings.sync_timeout = 1500
@@ -29,87 +29,87 @@ def sync_woocommerce():
     # apply minimal timeout of 60 sec
     if timeout < 60:
         timeout = 60
-	enqueue("woocommerceconnector.api.sync_woocommerce_resources", queue='long', timeout=timeout)
-	frappe.msgprint(_("Queued for syncing. It may take a few minutes to an hour if this is your first sync."))
+    enqueue("woocommerceconnector.api.sync_woocommerce_resources", queue='long', timeout=timeout)
+    frappe.msgprint(_("Queued for syncing. It may take a few minutes to an hour if this is your first sync."))
 
 @frappe.whitelist()
 def sync_woocommerce_resources():
-	woocommerce_settings = frappe.get_doc("WooCommerce Config")
+    woocommerce_settings = frappe.get_doc("WooCommerce Config")
 
-	make_woocommerce_log(title="Sync Job Queued", status="Queued", method=frappe.local.form_dict.cmd, message="Sync Job Queued")
-	
-	if woocommerce_settings.enable_woocommerce:
-		make_woocommerce_log(title="Sync Job Started", status="Started", method=frappe.local.form_dict.cmd, message="Sync Job Started")
-		try :
-			validate_woocommerce_settings(woocommerce_settings)
-			frappe.local.form_dict.count_dict = {}
-			frappe.local.form_dict.count_dict["customers"] = 0
-			frappe.local.form_dict.count_dict["products"] = 0
-			frappe.local.form_dict.count_dict["orders"] = 0
-			sync_products(woocommerce_settings.price_list, woocommerce_settings.warehouse, True if woocommerce_settings.sync_items_from_woocommerce_to_erp == 1 else False)
-			sync_customers()
-			sync_orders()
-			# close_synced_woocommerce_orders() # DO NOT GLOBALLY CLOSE
-			update_item_stock_qty()
-			frappe.db.set_value("WooCommerce Config", None, "last_sync_datetime", frappe.utils.now())
-			
-			make_woocommerce_log(title="Sync Completed", status="Success", method=frappe.local.form_dict.cmd, 
-				message= "Updated {customers} customer(s), {products} item(s), {orders} order(s)".format(**frappe.local.form_dict.count_dict))
+    make_woocommerce_log(title="Sync Job Queued", status="Queued", method=frappe.local.form_dict.cmd, message="Sync Job Queued")
+    
+    if woocommerce_settings.enable_woocommerce:
+        make_woocommerce_log(title="Sync Job Started", status="Started", method=frappe.local.form_dict.cmd, message="Sync Job Started")
+        try :
+            validate_woocommerce_settings(woocommerce_settings)
+            frappe.local.form_dict.count_dict = {}
+            frappe.local.form_dict.count_dict["customers"] = 0
+            frappe.local.form_dict.count_dict["products"] = 0
+            frappe.local.form_dict.count_dict["orders"] = 0
+            sync_products(woocommerce_settings.price_list, woocommerce_settings.warehouse, True if woocommerce_settings.sync_items_from_woocommerce_to_erp == 1 else False)
+            sync_customers()
+            sync_orders()
+            # close_synced_woocommerce_orders() # DO NOT GLOBALLY CLOSE
+            update_item_stock_qty()
+            frappe.db.set_value("WooCommerce Config", None, "last_sync_datetime", frappe.utils.now())
+            
+            make_woocommerce_log(title="Sync Completed", status="Success", method=frappe.local.form_dict.cmd, 
+                message= "Updated {customers} customer(s), {products} item(s), {orders} order(s)".format(**frappe.local.form_dict.count_dict))
 
-		except Exception as e:
-			if e.args[0] and hasattr(e.args[0], "startswith") and e.args[0].startswith("402"):
-				make_woocommerce_log(title="woocommerce has suspended your account", status="Error",
-					method="sync_woocommerce_resources", message=_("""woocommerce has suspended your account till
-					you complete the payment. We have disabled ERPNext woocommerce Sync. Please enable it once
-					your complete the payment at woocommerce."""), exception=True)
+        except Exception as e:
+            if e.args[0] and hasattr(e.args[0], "startswith") and e.args[0].startswith("402"):
+                make_woocommerce_log(title="woocommerce has suspended your account", status="Error",
+                    method="sync_woocommerce_resources", message=_("""woocommerce has suspended your account till
+                    you complete the payment. We have disabled ERPNext woocommerce Sync. Please enable it once
+                    your complete the payment at woocommerce."""), exception=True)
 
-				disable_woocommerce_sync_on_exception()
-			
-			else:
-				make_woocommerce_log(title="sync has terminated", status="Error", method="sync_woocommerce_resources",
-					message=frappe.get_traceback(), exception=True)
-					
-	elif frappe.local.form_dict.cmd == "woocommerceconnector.api.sync_woocommerce":
-		make_woocommerce_log(
-			title="woocommerce connector is disabled",
-			status="Error",
-			method="sync_woocommerce_resources",
-			message=_("""woocommerce connector is not enabled. Click on 'Connect to woocommerce' to connect ERPNext and your woocommerce store."""),
-			exception=True)
+                disable_woocommerce_sync_on_exception()
+            
+            else:
+                make_woocommerce_log(title="sync has terminated", status="Error", method="sync_woocommerce_resources",
+                    message=frappe.get_traceback(), exception=True)
+                    
+    elif frappe.local.form_dict.cmd == "woocommerceconnector.api.sync_woocommerce":
+        make_woocommerce_log(
+            title="woocommerce connector is disabled",
+            status="Error",
+            method="sync_woocommerce_resources",
+            message=_("""woocommerce connector is not enabled. Click on 'Connect to woocommerce' to connect ERPNext and your woocommerce store."""),
+            exception=True)
 
 def validate_woocommerce_settings(woocommerce_settings):
-	"""
-		This will validate mandatory fields and access token or app credentials 
-		by calling validate() of WooCommerce Config.
-	"""
-	try:
-		woocommerce_settings.save()
-	except woocommerceError:
-		disable_woocommerce_sync_on_exception()
+    """
+        This will validate mandatory fields and access token or app credentials 
+        by calling validate() of WooCommerce Config.
+    """
+    try:
+        woocommerce_settings.save()
+    except woocommerceError:
+        disable_woocommerce_sync_on_exception()
 
 @frappe.whitelist()
 def get_log_status():
-	log = frappe.db.sql("""select name, status from `tabwoocommerce Log` 
-		order by modified desc limit 1""", as_dict=1)
-	if log:
-		if log[0].status=="Queued":
-			message = _("Last sync request is queued")
-			alert_class = "alert-warning"
-		elif log[0].status=="Error":
-			message = _("Last sync request was failed, check <a href='../desk#Form/woocommerce Log/{0}'> here</a>"
-				.format(log[0].name))
-			alert_class = "alert-danger"
-		else:
-			message = _("Last sync request was successful")
-			alert_class = "alert-success"
-			
-		return {
-			"text": message,
-			"alert_class": alert_class
-		}
-		
+    log = frappe.db.sql("""select name, status from `tabwoocommerce Log` 
+        order by modified desc limit 1""", as_dict=1)
+    if log:
+        if log[0].status=="Queued":
+            message = _("Last sync request is queued")
+            alert_class = "alert-warning"
+        elif log[0].status=="Error":
+            message = _("Last sync request was failed, check <a href='../desk#Form/woocommerce Log/{0}'> here</a>"
+                .format(log[0].name))
+            alert_class = "alert-danger"
+        else:
+            message = _("Last sync request was successful")
+            alert_class = "alert-success"
+            
+        return {
+            "text": message,
+            "alert_class": alert_class
+        }
+        
 @frappe.whitelist()
 def sync_woocommerce_ids():
-	"Enqueue longjob for syncing woocommerce"
-	enqueue("woocommerceconnector.sync_products.add_w_id_to_erp", queue='long', timeout=1500)
-	frappe.msgprint(_("Queued for syncing. It may take a few minutes to an hour if this is your first sync."))
+    "Enqueue longjob for syncing woocommerce"
+    enqueue("woocommerceconnector.sync_products.add_w_id_to_erp", queue='long', timeout=1500)
+    frappe.msgprint(_("Queued for syncing. It may take a few minutes to an hour if this is your first sync."))
